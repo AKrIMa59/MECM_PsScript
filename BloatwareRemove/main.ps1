ce21554deadb362e7a06fcd22661de2a889783a5
Luc Bourbiaux <bourbiauxluc@gmail.com>
2022-05-20 15:40:55 +0200

    MAJ

--- BloatwareRemove/main.ps1
+++ BloatwareRemove/main.ps1
@@ -1,5 +1,6 @@
 $bloatware_file= Get-Content .\bloatware-list.txt
+#On vérifie si winget est présent sur le pc
 foreach ($line in $bloatware_file) {
     Write-Output $line
-    winget $line --accept-source-agreements
+    winget uninstall $line --accept-source-agreements
 }
--- ConfigSystem/main.ps1
+++ ConfigSystem/main.ps1
@@ -5,13 +5,36 @@ powercfg /H off
 
 
 #activation de SMBv1 si désactivé
-$SMB1=Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol-Client
+$SMB1=Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
 if ("Disabled" -eq $SMB1.State) {
-    Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol-Client -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart
+    Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol-Server -NoRestart
+}
+
+#activation de telnet si désactivé
+$Telnet=Get-WindowsOptionalFeature -Online -FeatureName TelnetClient
+if ("Disabled" -eq $Telnet.State) {
+    Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient -NoRestart
+}
+
+#désactivation de Printing-XPSServices-Features
+$PrintingXPSServicesFeatures=Get-WindowsOptionalFeature -Online -FeatureName Printing-XPSServices-Features
+if ("Enabled" -eq $PrintingXPSServicesFeatures.State) {
+    Disable-WindowsOptionalFeature -Online -FeatureName Printing-XPSServices-Features -NoRestart
 }
 
 #Activation de .NET 3.5
-DISM /Online /Enable-Feature /FeatureName:NetFx3 /All /quiet /NoRestart
+$NetFx3=Get-WindowsOptionalFeature -Online -FeatureName NetFx3
+if ($NetFx3.State -like "*Disabled*") {
+    Set-Itemproperty -path 'HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU' -Name "UseWUServer" -Value 0
+    Restart-Service -Name "wuauserv"
+    Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -NoRestart
+    Set-Itemproperty -path 'HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU' -Name "UseWUServer" -Value 1
+    Restart-Service -Name "wuauserv"
+}
+
+#Désactivation de la gestion des imprimantes par windows
+Set-Itemproperty -path 'HKCU:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows' -Name "LegacyDefaultPrinterMode" -Value 1
 
 #Activation registre à distance
 Set-Service -Name RemoteRegistry -StartupType Automatic
